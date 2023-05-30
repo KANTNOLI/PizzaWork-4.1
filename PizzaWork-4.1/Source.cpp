456c22460a5a80b96f1fc8d5f92c46d5d9c127db
KANTNOLI <zahargysev02@gmail.com>
2023-05-30 13:58:37 +0300

    Ready

--- PizzaWork-4.1/Source.cpp
+++ PizzaWork-4.1/Source.cpp
@@ -11,26 +11,33 @@ using namespace std;
 string FillStr(string mainString, int size) {
 	string msg = "";
 	int sizeMain = 0;
-	int 
 	for (sizeMain = 0; sizeMain < mainString[sizeMain]; sizeMain++);
+	int remainder = size % sizeMain; // 10 % 3 = 1
+	int count = size / sizeMain; // 10 / 3 = 3 
 
-	for (int i = 0; i < size; i++)
-	{
-		if () {
-
-		}
+	for (int i = 0; i < count; i++) {
+		msg += mainString;
 	}
 
+	for (int i = 0; i < remainder; i++) {
+		msg += mainString[i];
+	}
 
 	return msg;
 }
 
 int main() {
+	string str1 = "abasc/";
+	string str2 = "vasf/";
+	string str3 = "qds/";
+	string str4 = "ii/";
+	string str5 = "c/";
 
-	string a = "123";
-	string b = a + a[1];
-
-	cout << b << endl;
-
+	cout << FillStr(str1, 10) << endl;
+	cout << FillStr(str2, 10) << endl;
+	cout << FillStr(str3, 10) << endl;
+	cout << FillStr(str4, 10) << endl;
+	cout << FillStr(str5, 10) << endl;
+	
 	return 0;
 }
