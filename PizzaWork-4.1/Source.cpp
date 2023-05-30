b87b4992c222bdc1f696d8713d44baa293d2d3b3
KANTNOLI <zahargysev02@gmail.com>
2023-05-30 13:40:54 +0300

    Add project files.

---
+++ PizzaWork-4.1/Source.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+
+//—оздать функцию FillStr(S, N) строкового типа, возвращающую строку длины N,
+//заполненную повтор€ющимис€ копи€ми строки - шаблона S(последн€€ копи€
+//cтроки - шаблона может входить в результирующую строку частично).»спользу€
+//эту функцию, сформировать по данному числу N и п€ти данным строкамшаблонам п€ть результирующих строк длины N.
+
+string FillStr(string mainString, int size) {
+	string msg = "";
+	int sizeMain = 0;
+	int 
+	for (sizeMain = 0; sizeMain < mainString[sizeMain]; sizeMain++);
+
+	for (int i = 0; i < size; i++)
+	{
+		if () {
+
+		}
+	}
+
+
+	return msg;
+}
+
+int main() {
+
+	string a = "123";
+	string b = a + a[1];
+
+	cout << b << endl;
+
+	return 0;
+}
